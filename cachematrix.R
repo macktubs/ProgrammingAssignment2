1b7e096a5016a9cd85adceaeb35379ac28b8e263
macktubs <macktubs@gmail.com>
2020-12-09 13:25:29 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,24 @@
 makeCacheMatrix <- function(x = matrix()){
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
+  ## This function creates a special "matrix" object that can cache its inverse
+  inv <- NULL               ## initializes inv as NULL; will hold value of matrix inverse
+  set <- function(y){       ## value of matrix in parent environment
+    x <<- y                 ## value of matrix in parent environment
+    inv <<- NULL            ## if there is a new matrix, reset inv to NULL
   }
-  get <- function() {x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  get <- function() {x}     ## defines the get function - returns value of the matrix argument
+  setInverse <- function(inverse) {inv <<- inverse}    ## assigns value of inv in parent environment
+  getInverse <- function() {inv}                       ## gets the value of inv where called
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ## you need this in order to refer 
+                                                                               ## to the functions with the $ operator
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
+
+
 cacheSolve <- function(x, ...){
+  ## Returns a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if(!is.null(inv)){
     message("getting cached data")
